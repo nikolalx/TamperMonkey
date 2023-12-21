b4149df5011c2a857a092dba1e85eeb0f2493c8e
nikolalx <nikilx123@gmail.com>
2023-12-21 01:29:13 +0100

    changing to github repo

--- DashboardReady/main.js
+++ DashboardReady/main.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Jira Dashboard Ready Tickets
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.5
+// @description  Depict the number of ready ticket in red.
+// @author       Nikola Aleksic
 // @match        https://jiradg.atlassian.net/jira/dashboards/25522
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atlassian.net
 // @grant        none
+// @downloadURL  https://gist.github.com/nikolalx/10f6607b8167698655f554312c19ba15
+// @updateURL    https://gist.github.com/nikolalx/10f6607b8167698655f554312c19ba15
 // ==/UserScript==
 
 window.addEventListener("load", e => {
     'use strict'
     const categorySelector1 = document.querySelector('a[href="https://jiradg.atlassian.net/issues/?jql=issuetype+%3D+%22Product+Data+Integration%22+AND+%28labels+is+EMPTY+OR+labels+not+in+%28template%2C+test%29%29+AND+status+not+in+%28Planning%2C+Closed%2C+%22Data+Preparation%22%2C+Monitoring%2C+Integration%29+AND+%22CM-Team%5BDropdown%5D%22+%3D+Mobiles+AND+status+%3D+Ready"]')
-    if(categorySelector1){
+    if(categorySelector1){ //if the const categorySelector1 exists, run all.
 
         console.log(Boolean(categorySelector1))
 
